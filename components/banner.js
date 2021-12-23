15401b825707753333267a309de304550619e955
zkula <zachkula@hotmail.com>
2021-12-23 10:02:19 -0500

    Set up home page of app

---
+++ components/banner.js
@@ -0,0 +1,18 @@
+import styles from "./Banner.module.css";
+
+const Banner = ({ buttonText, onClick }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <span className={styles.title1}>Coffee</span>
+        <span className={styles.title2}>Connoisseur</span>
+      </h1>
+      <p className={styles.subtitle}>Discover your local coffee shops!</p>
+      <button className={styles.button} onClick={onClick}>
+        {buttonText}
+      </button>
+    </div>
+  );
+};
+
+export default Banner;
