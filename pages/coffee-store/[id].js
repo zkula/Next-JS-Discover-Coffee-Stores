530b932bb6f757fca7ce5fe00de11ae518dda9f1
zkula <zachkula@hotmail.com>
2021-12-24 16:05:16 -0500

    Render coffee store name as browser title

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import Head from "next/head";
 
 import coffeeStoresData from "../../data/coffee-stores.json";
 
@@ -29,19 +30,30 @@ export function getStaticPaths() {
         },
       },
     ],
-    fallback: false,
+    fallback: true,
   };
 }
 
-const CoffeeStore = () => {
+const CoffeeStore = ({ coffeeStore }) => {
   const router = useRouter();
   const { id } = router.query;
+  const { name, address, neighbourhood } = coffeeStore;
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      <p>Coffee Store Page {id}</p>
+      <Head>
+        <title>{name}</title>
+      </Head>
       <Link href="/">
         <a>Back to home</a>
       </Link>
+      <p>{address}</p>
+      <p>{name}</p>
+      <p>{neighbourhood}</p>
     </div>
   );
 };
